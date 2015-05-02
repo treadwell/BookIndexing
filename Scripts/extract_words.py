2cb2015f470e1953ac0726760f093fe042191944
treadwell <brooks.ken@gmail.com>
2015-05-02 10:50:47 -0400

    Demonstrate word filtering functions

--- Scripts/extract_words.py
+++ Scripts/extract_words.py
@@ -1,3 +1,5 @@
+﻿import os
+
 from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
@@ -6,10 +8,20 @@ from cStringIO import StringIO
 import nltk
 from nltk.corpus import stopwords
 
+working_dir = os.path.dirname(os.path.abspath(__file__))
+filename = "Think Complexity - Allen B. Downey.pdf"
+path = os.path.join(working_dir, filename)
+
+# files = []
+# for f in os.listdir(working_dir):
+#     if f.endswith(".pdf"):
+#         files.append(f)
+
+
 def convert_pdf_to_txt(path):
     rsrcmgr = PDFResourceManager()
     retstr = StringIO()
-    codec = 'utf-8'  # utf-8
+    codec = 'utf-8'
     laparams = LAParams()
     device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
     fp = file(path, 'rb')
@@ -26,22 +38,9 @@ def convert_pdf_to_txt(path):
     retstr.close()
     return str
 
-def whatisthis(s):
-    if isinstance(s, str):
-        print "acii"
-    elif isinstance(s, unicode):
-        print "unicode"
-    else:
-        print "not a string"
 
 
-#path = "/Users/kbrooks/Documents/Book indexing project/Test Library/Allen B. Downey/Think Complexity (11)/Think Complexity - Allen B. Downey.pdf"
-path = "/Users/kbrooks/Documents/Book indexing project/Test Library/Cosma Shalizi/split, apply, combine with plyr (1)/split, apply, combine with plyr - Cosma Shalizi.pdf"
-
-print "Starting PDF extraction..."
 book = convert_pdf_to_txt(path)
-
-print "Starting tokenizing..."
 
 # 1. for each document of the corpus:
 # - tokenize
@@ -51,16 +50,9 @@ text = nltk.Text(tokens)
 # - toLowerCase, trim
 words = [w.lower() for w in text]
 
-# Test for unicode
-
-#unicode_test = [whatisthis(w) for w in words]
-#print unicode_test
-
-#words = [w.encode('utf-16')  for w in words]
-
 # - replace \ with space
 
-words = [w.replace("\\", " ") for w in words]  # I think the problem is that I'm trying to replace unicode
+words = [w.replace("\\", " ") for w in words]
 
 # - delete all non printable characters with a regex
 # - trim again
@@ -77,18 +69,18 @@ words = [w.replace("\\", " ") for w in words]  # I think the problem is that I'm
 
 # 4. remove n-grams which appear just once or twice (unjustified but reasonable absolute cut-off, help to clean a lot!)
 
-print "Removing stopwords..."
 # 5. remove stop words
 # //there are many criteria here, but the main ones are:
 # - if it is a unigram, remove it if it is in the list of stopwords
 # - if it is a bi-gram or above, remove it IF some of its token belongs to the list of stopwords
 
-stopwords = set(stopwords.words('english'))
-#stopword_test = [whatisthis(w) for w in stopwords]
+stopwords = set(stopwords.words('english')) ## O(1) instead of O(n)
+def word_filter(word):
+    return (word.isalpha() and
+            len(word) <= 20 and
+            word not in stopwords)
 
-#print stopword_test
-
-newwords = [w for w in words if w not in stopwords]
+newwords = [w for w in words if word_filter(w)]
 
 # 6. keep only the n most frequent n-grams (n depends on the size of your corpus and your goals)
 
@@ -100,12 +92,14 @@ newwords = [w for w in words if w not in stopwords]
 
 # 9. proceed with the rest of the text analysis!
 
-# As you see, I found that the stopwords removal should arrive quite late in the steps, and in any case after the detection of n-grams. 
-# Also, I improved a lot my results by fine tuning my list of stopwords, and by creating several lists of stopwords. You should not 
-# hesitate to use a long list I believe, in my case I have excellent results with about 5500 stopwords. This is a topic in itself, 
-# we should discuss it some time!
+# As you see, I found that the stopwords removal should arrive quite late in the steps, and in any case after the detection of n-grams. Also, I improved a lot my results by fine tuning my list of stopwords, and by creating several lists of stopwords. You should not hesitate to use a long list I believe, in my case I have excellent results with about 5500 stopwords. This is a topic in itself, we should discuss it some time!
 
 
-vocab = sorted(set(newwords))
+# vocab = sorted(set(newwords))
+# vocab = nltk.FreqDist(newwords)
 
-print vocab
+from collections import Counter
+c = Counter(newwords)
+
+print c
+import ipdb; ipdb.set_trace()
