d4a2ffd6b672c279f9328a89b2be5cf9fdb1da3c
treadwell <brooks.ken@gmail.com>
2015-05-04 14:22:34 -0400

    Add frequency analysis to invalid word list

--- Scripts/test_english.py
+++ Scripts/test_english.py
@@ -1,31 +1,16 @@
+import nltk
 from nltk.corpus import words
+import json
 
 # - text for English words
 
-text_words = ["built", 
-        "buntine", 
-        "c", 
-        "california", 
-        "called", 
-        "cally", 
-        "candidate", 
-        "carolina", 
-        "case", 
-        "categorical", 
-        "cation", 
-        "celine", 
-        "centered", 
-        "cex", 
-        "changing", 
-        "chical", 
-        "choice", 
-        "choose", 
-        "chosen", 
-        "christian", 
-        "clark", 
-        "class", ]
 
-print len(words.words())  # not big enough
+title_dict = json.load(open('../data/file_data.json'))
+
+text_words = title_dict["words"]
+
+
+print len(words.words())  # not big enough - I need a much better dictionary
 
 English_words = [w.lower() for w in words.words()]
 
@@ -35,4 +20,8 @@ text_valid =  [w for w in text_words if w in English_words]
 
 text_invalid =  [w for w in text_words if w not in English_words]  # missing some obvious ones
 
-print text_invalid
+#print text_invalid
+invalid_text_freq = nltk.FreqDist(text_invalid)
+tenMostCommon_invalid = invalid_text_freq.most_common(10)
+
+print tenMostCommon_invalid
